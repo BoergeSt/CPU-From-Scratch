16099129d9824f4277e8dd7a4a98ed44a019cefa
Boerge Struempfel <boerge.struempfel@gmail.com>
2026-08-15 00:46:32 +0200

    refactor(hydrogen): alphabetize interface/module ordering
    Groups interface instance declarations and module instantiations each
    alphabetically, ahead of wiring in the D-bus interconnect and UART.
    No functional change.
    
    Signed-off-by: Boerge Struempfel <boerge.struempfel@gmail.com>

--- machines/hydrogen/rtl/hydrogen.sv
+++ machines/hydrogen/rtl/hydrogen.sv
@@ -2,25 +2,23 @@ module hydrogen (
     input logic clk_i,
     input logic rst_i
 );
-  flow_ctl_if flow_ctl_if ();
-  flow_ctl flow_ctl (
-      .clk_i(clk_i),
-      .rst_i(rst_i),
-      .bus  (flow_ctl_if.flow_ctl)
-  );
-
   alu_if alu_if ();
+  alu_status_if alu_status_if ();
+  bus_if bus_I ();
+  bus_if bus_D ();
+  flow_ctl_if flow_ctl_if ();
+  regfile_read_if read_1 ();
+  regfile_read_if read_2 ();
+  regfile_read_if read_3 ();
+  regfile_write_if write ();
+
   alu alu (.bus(alu_if));
 
-  alu_status_if alu_status_if ();
   alu_status alu_status (
       .clk_i(clk_i),
       .rst_i(rst_i),
       .bus  (alu_status_if)
   );
-
-  bus_if bus_I ();
-  bus_if bus_D ();
 
   bram #(
       .Size(32'h1000)
@@ -28,21 +26,6 @@ module hydrogen (
       .clk_i(clk_i),
       .bus_I(bus_I),
       .bus_D(bus_D)
-  );
-
-
-  regfile_read_if read_1 ();
-  regfile_read_if read_2 ();
-  regfile_read_if read_3 ();
-  regfile_write_if write ();
-
-  regfile regfile (
-      .clk_i(clk_i),
-      .rst_i(rst_i),
-      .read1(read_1),
-      .read2(read_2),
-      .read3(read_3),
-      .write(write)
   );
 
   core_glue core_glue (
@@ -55,6 +38,21 @@ module hydrogen (
       .flow_ctl  (flow_ctl_if),
       .bus_I     (bus_I),
       .bus_D     (bus_D)
+  );
+
+  flow_ctl flow_ctl (
+      .clk_i(clk_i),
+      .rst_i(rst_i),
+      .bus  (flow_ctl_if.flow_ctl)
+  );
+
+  regfile regfile (
+      .clk_i(clk_i),
+      .rst_i(rst_i),
+      .read1(read_1),
+      .read2(read_2),
+      .read3(read_3),
+      .write(write)
   );
 
 endmodule
