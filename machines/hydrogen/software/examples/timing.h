79e512b91dd203d7954a9b4c09fc29a9b03ac4b9
Boerge Struempfel <boerge.struempfel@gmail.com>
2026-08-17 19:40:50 +0200

    feat(hydrogen): rewrite uart-hello-world on shared uart.S/time.S helpers
    Factors UartSetup/GetCharacter/PrintCharacter/Print out of the example
    into uart.S, and WaitCycles into time.S, so both example programs can
    share them instead of duplicating UART bring-up and byte-by-byte
    transmit code inline. hydrogen.h supersedes uart_include.h and
    timing.h, which are now unused.
    
    Signed-off-by: Boerge Struempfel <boerge.struempfel@gmail.com>

--- machines/hydrogen/software/examples/timing.h
+++
@@ -1,3 +0,0 @@
-#define CLOCK_SPEED 10000000 // simulation locked to 10MHz (max with current design about 13MHz)
-
-#define UART_DIV 5 // 10MHz / 16 oversampling / 115200 target baud = 5.4 -- does not matter for simulation as receiver uses the same div.
