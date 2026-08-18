cd149932cee0bf22da8795c5a82d2a111bfeb1ad
Boerge Struempfel <boerge.struempfel@gmail.com>
2026-08-18 16:17:21 +0200

    feat(hydrogen): add STACK_ALLOC/STACK_FREE, rebuild PUSH/POP on top
    STACK_ALLOC(n)/STACK_FREE(n) move STACK_REG by n words directly --
    reserving scratch stack space or discarding pushed values without a
    register-carrying PUSH/POP round trip. PUSH/POP now call them instead
    of hand-adjusting STACK_REG by 1 each. Also renames CALL's internal
    per-call return-label prefix from ret_ to Call_Return_.
    
    Signed-off-by: Boerge Struempfel <boerge.struempfel@gmail.com>

--- machines/hydrogen/software/examples/include/calling_convention.h
+++ machines/hydrogen/software/examples/include/calling_convention.h
@@ -7,12 +7,15 @@
 #define RET_REG R7
 #define STACK_REG R6
 
-#define PUSH(reg) sub STACK_REG, STACK_REG, 1; store [STACK_REG], reg;
-#define POP(reg) load reg, [STACK_REG]; add STACK_REG, STACK_REG, 1;
+#define STACK_ALLOC(n) sub STACK_REG, STACK_REG, n
+#define STACK_FREE(n) add STACK_REG, STACK_REG, n
+
+#define PUSH(reg) STACK_ALLOC(1); store [STACK_REG], reg;
+#define POP(reg) load reg, [STACK_REG]; STACK_FREE(1);
 
 // Nested CALLs: saving RET_REG before the inner call is on the caller, same as any callee-saved register.
 #define CALL_IMPL(func,return_label) imm_set RET_REG, return_label ;always [func]; return_label:
-#define CALL(func) CALL_IMPL(func,CONCAT(ret_,__COUNTER__))
+#define CALL(func) CALL_IMPL(func,CONCAT(Call_Return_,__COUNTER__))
 
 #define RETURN always [RET_REG]
 
